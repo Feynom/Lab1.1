09685959cab971dd1d41ebc14c6a7a25d5138994
Feynom <53947611+Feynom@users.noreply.github.com>
2021-02-22 20:04:39 +0200

    Add files via upload

---
+++ UnitTest1.1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab1.1/Lab1.1/FloatRange.h"
+#include "../Lab1.1/Lab1.1/FloatRange.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest11
+{
+	TEST_CLASS(UnitTest11)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			FloatRange a, b, c, d;
+			a.Init(1, 2);
+			b.Init(1, 10);
+			c.Init(-100, -5);
+			d.Init(5, 600);			
+			Assert::IsTrue(a.rangeCheck(1.5));
+			Assert::IsTrue(b.rangeCheck(5));
+			Assert::IsFalse(c.rangeCheck(6));
+			Assert::IsFalse(d.rangeCheck(4));
+		}
+	};
+}
